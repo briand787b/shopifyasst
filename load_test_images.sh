22286df375a478b70fb59af6f3404800839b0bd1
Brian D'Ostilio <briand787b@gmail.com>
2023-01-07 16:46:03 -0500

    test image uploader can make multi copies

--- load_test_images.sh
+++ load_test_images.sh
@@ -5,6 +5,8 @@ source .env
 
 # cli args
 #
+# COUNT is the number of copies to create from each original image
+COUNT=1
 # REMOVE being non-empty string indicates that the AWS bucket should
 # be cleaned before any new file(s) are uploaded
 REMOVE=
@@ -21,13 +23,14 @@ declare -a FILES
 
 function usage ()
 {
-    printf "usage: %s [-h] [-d IMG_SRC_DIR] [-r] [-s | file1 ... filen]\n
+    printf "usage: %s [-h] [-c COUNT] [-d IMG_SRC_DIR] [-r] [-s | file1 ... filen]\n
 
 description: Upload jpeg from local system to S3 bucket that shopifyasst can
     pull from.  Ensure that you have a valid .env file to pull configs from.
 
 flags:
     -h: print this help info
+    -c: number of copies to create from each original image
     -d: provide directory path to search for images.  Defaults to '$IMG_SRC_DIR'
     -r: remove all existant files, if any exist, from the bucket
     -s: grabs smallest image in searched dir.  Exclusive to file args
@@ -44,6 +47,9 @@ examples:
     Upload smallest image in $IMG_SRC_DIR, cleaning aws bucket beforehand
     %s -r -s
 
+    Upload smallest image in $IMG_SRC_DIR 5 times
+    %s -s -c 5
+
     Upload all images from specified dir
     %s -d /path/to/images
 
@@ -52,15 +58,27 @@ examples:
     \n" ${0##*/} ${0##*/} ${0##*/} ${0##*/} ${0##*/} >& 2
 }
 
-while getopts ':hrsd:' OPTION
+while getopts ':hc:d:rs' OPTION
 do
     case $OPTION in
         h)
             usage
             exit 0
         ;;
+        c)
+            COUNT="$OPTARG"
+            re='^[0-9]+$'
+            if ! [[ "$COUNT" =~ $re ]] ; then
+                printf "ERROR: -c arg '%s' is not a number\n" $COUNT
+                exit 1
+            fi
+        ;;
         d)
             IMG_SRC_DIR="$OPTARG"
+            if ! [ -d "$IMG_SRC_DIR" ]; then
+                echo "ERROR: '$IMG_SRC_DIR' is not a valid directory"
+                exit 3
+            fi
         ;;
         r)
             REMOVE=TRUE
@@ -81,11 +99,6 @@ if [ -n "$SMALLEST" -a -n "$*" ]; then
     usage
     printf "\nERROR: cannot specify '-s' flag and file arg(s) simultaneously\n"
     exit 2
-fi
-
-if ! [ -d "$IMG_SRC_DIR" ]; then
-    echo "ERROR: '$IMG_SRC_DIR' does not exist or is not a directory"
-    exit 3
 fi
 
 if [ -z "$*" ]; then
@@ -111,27 +124,29 @@ if [ -z "$FILES" ]; then
     exit 5
 fi
 
+if [ -n "$(aws s3 ls $S3_IMAGE_BUCKET)" -a -n "$REMOVE" ]; then
+    echo "WARNING: You will delete all files in S3 bucket $S3_IMAGE_BUCKET"
+    read -p 'Delete all files in bucket [y/N]: ' DELETE
+    DELETE="$(echo "$DELETE" | tr '[:lower:]' '[:upper:]')"
+    if [ "$DELETE" != 'Y' ]; then
+        echo cannot proceed with non-empty bucket
+        exit 6
+    fi
+    
+    aws s3 rm $S3_IMAGE_BUCKET --recursive
+fi
+
 echo "${#FILES[@]} file(s) will be uploaded to AWS"
 for FILE in "${FILES[@]}"; do
-    echo "copying file '$FILE' to AWS"
-
-    if [ -n "$(aws s3 ls $S3_IMAGE_BUCKET)" -a -n "$REMOVE" ]; then
-        echo "WARNING: You will delete all files in S3 bucket $S3_IMAGE_BUCKET"
-        read -p 'Delete all files in bucket [y/N]: ' DELETE
-        DELETE="$(echo "$DELETE" | tr '[:lower:]' '[:upper:]')"
-        if [ "$DELETE" != 'Y' ]; then
-            echo cannot proceed with non-empty bucket
-            exit 6
-        fi
-        
-        aws s3 rm $S3_IMAGE_BUCKET --recursive
-    fi
-
+    echo "copying file '$FILE' to AWS $COUNT time(s)"
+    
     exiftool -Subject='louisiana, new orleans' "$FILE"
     rm "${FILE}_original" # exiftool creates this
 
-    UUID=$(uuidgen)
-    aws s3 cp "$FILE" "$S3_IMAGE_BUCKET/$UUID.jpg"
+    for (( c=1; c<="$COUNT"; c++ )); do
+        UUID=$(uuidgen)
+        aws s3 cp "$FILE" "$S3_IMAGE_BUCKET/$UUID.jpg"
+    done
 done
 
 echo exiting successfully...
