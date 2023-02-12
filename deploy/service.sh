6973e6aff56b495dbcc167e0afcee863d39aa063
Brian D'Ostilio <briand787b@gmail.com>
2023-02-12 10:47:14 -0500

    adds credentials file to aws env

--- deploy/service.sh
+++ deploy/service.sh
@@ -3,5 +3,6 @@
 # The script that the systemd service executes
 
 export AWS_CONFIG_FILE=/home/ubuntu/.aws/config
+export AWS_SHARED_CREDENTIALS_FILE=/home/ubuntu/.aws/credentials
 cd /home/ubuntu/shopifyasst
 ./main.sh
